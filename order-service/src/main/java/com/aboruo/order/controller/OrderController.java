f13b3e8b3cd695017f140e6371a3fc6ca0d5126c
zhaoxinbo <zhaoxinbo@cm-inv.com>
2018-05-02 18:43:15 +0800

    添加了熔断机制，zuul与ribbon的配合

--- order-service/src/main/java/com/aboruo/order/controller/OrderController.java
+++ order-service/src/main/java/com/aboruo/order/controller/OrderController.java
@@ -1,13 +1,40 @@
 package com.aboruo.order.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.aboruo.order.service.ProducerService;
+/**
+	 * @Title:  OrderController
+	 * @Description:  订单处理类
+	 * @author:  aboruo
+	 * @date 2018年5月2日 下午6:33:41
+ */
 @RestController
 public class OrderController {
+	@Autowired
+	private ProducerService producerService;
+	/**
+		 * @Title: hello
+		 * @Description:  接口测试方法
+		 * @author: aboruo
+		 * @return
+		 * @date 2018年5月2日 下午6:34:04
+	 */
 	@RequestMapping(value="/hello",method= {RequestMethod.GET,RequestMethod.POST})
 	public String hello() {
 		return "hello,this is order-service";
+	}
+	/**
+		 * @Title: callProducerService
+		 * @Description:  请求产品生产服务
+		 * @author: aboruo
+		 * @return
+		 * @date 2018年5月2日 下午6:36:38
+	 */
+	@RequestMapping(value="/product/hello",method= {RequestMethod.GET,RequestMethod.POST})
+	public String callProducerService() {
+		return producerService.callProducerService();
 	}
 }
