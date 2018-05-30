1c764fc9a7a578d79e0944e31241b9ca67dd8aaf
zhaoxinbo <zhaoxinbo@cm-inv.com>
2018-05-30 19:24:19 +0800

    配置服务中心修改为rabbitmq配置

--- rabbitstream-hello/src/main/java/com/aboruo/rabbitmq/receiver/SinkReceiver.java
+++ rabbitstream-hello/src/main/java/com/aboruo/rabbitmq/receiver/SinkReceiver.java
@@ -5,8 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
-@EnableBinding(Sink.class)
+@EnableBinding(value = {Sink.class,Source.class})
 public class SinkReceiver {
 	private static Logger logger = LoggerFactory.getLogger(SinkReceiver.class);
 	@StreamListener(Sink.INPUT)
